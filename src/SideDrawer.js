a22be4c1bc863fee0a5536989793e2e27b4fcded
David J. Sanderson <sanderdj90@gmail.com>
2020-03-18 22:16:36 -0600

    feat: sidebar appears and disappears

--- src/SideDrawer.js
+++ src/SideDrawer.js
@@ -1,45 +1,66 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import classNames from "classnames";
 const SideDrawer = () => {
-
-    const handleClick = (e) => console.log(e.target)
-    return (
-        <div>
-        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
-          <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <span className="font-bold text-xl">Menu Icon</span>
-          </div>
-    
-          <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
-            <div className="text-sm sm:flex-grow">
-
-            </div>
-            <div>
-              <a
-                href="#"
-                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0"
-                >Login</a
-              >
-            </div>
-          </div>
-        </nav>
-        <div className="w-56 absolute left-0 top-0 h-full bg-orange-600">
-<div className="pl-8 py-6 font-bold"><span className="px-2"><button onClick={handleClick} href="">BACK</button></span>JAMIE BURNES</div>
-<div className="flex-col flex-grow text-center">
-<hr className="py-2"/>
-    <div>Home</div>
-    <div>Bio</div>
-    <div>Portfolio</div>
-    <div>Show Announcement</div>
-    <div>Recent Articles</div>
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = e => setIsOpen(!isOpen);
+  let sideBarClass = classNames({
+    hidden: !isOpen,
+    block: isOpen
+  });
+  return (
+    <div>
+      <nav className="flex items-center justify-between flex-wrap bg-yellow-100">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-bold text-xl">
+            <button onClick={handleClick} className="p-5">
+              <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="100" height="20"></rect>
+                <rect y="30" width="100" height="20"></rect>
+                <rect y="60" width="100" height="20"></rect>
+              </svg>
+            </button>
+          </span>
         </div>
-</div>
-      
-    
 
-
+        <div className="flex flex-row mr-16">
+          <div className="bg-indigo-500">
+            <a href="mailto:jamie@jamieburnes.com">
+              <span>jamie@jamieburnes.com</span>
+            </a>
+          </div>
+          <div className="bg-indigo-500">
+            <a
+              href="https://www.instagram.com/jamieburnessculpture/"
+              target="_blank"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0"
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div
+        className={`${sideBarClass} w-56 absolute left-0 top-0 h-full bg-yellow-100`}
+      >
+        <div className="pl-8 py-5 font-bold">
+          <span className="px-2">
+            <button onClick={handleClick} href="">
+              BACK
+            </button>
+          </span>
+          JAMIE BURNES
+        </div>
+        <div className="flex-col flex-grow text-center">
+          <hr />
+          <div className="py-4">Home</div>
+          <div className="py-4">Bio</div>
+          <div className="py-4">Portfolio</div>
+          <div className="py-4">Show Announcement</div>
+          <div className="py-4">Recent Articles</div>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default SideDrawer
+export default SideDrawer;
