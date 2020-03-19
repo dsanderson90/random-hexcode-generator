8349485ab55c9d247105cb66d42a6ac4eeb759c7
David J. Sanderson <sanderdj90@gmail.com>
2020-03-18 20:53:03 -0600

    chore: class >> className

---
+++ src/SideDrawer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const SideDrawer = () => {
+    return (
+        <div>
+        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <span className="font-bold text-xl">Menu Icon</span>
+          </div>
+    
+          <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
+            <div className="text-sm sm:flex-grow">
+
+            </div>
+            <div>
+              <a
+                href="#"
+                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0"
+                >Login</a
+              >
+            </div>
+          </div>
+        </nav>
+        <div className="">
+side drawer
+
+        </div>
+      
+    
+
+
+    </div>
+    )
+}
+
+export default SideDrawer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: '2rem',
+    }
   },
   variants: {},
   plugins: []
